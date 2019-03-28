02ecd3cd9d869c114b1a5e838b2e9a3c85e7fd9b
stackninigi <stackninigi@gmail.com>
2019-03-28 09:09:56 +0100

    [MS-003] calendar - not ready, have to commit due to hardware change

--- screens/Simulation/SimulationScreenComponent.js
+++ screens/Simulation/SimulationScreenComponent.js
@@ -5,10 +5,25 @@ import {
     View,
 } from 'react-native';
 import {Button} from "react-native-elements";
+import {RkTabView} from 'react-native-ui-kitten';
+import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
 
 import MyText from '../../components/MyText/MyText';
 
 export default class SimulationScreenComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            markedDates: {
+                '2019-04-23': {selected: true, endingDay: true, color: 'green', textColor: 'gray'},
+            },
+        };
+    }
+
+    onDateSelect = date => {
+        console.log('date', date)
+    };
+
     render() {
         return <View style={styles.container}>
             <Text style={styles.title}>Start simulation</Text>
@@ -16,9 +31,34 @@ export default class SimulationScreenComponent extends React.Component {
             and if you like them, just press a button to start simulation! Be careful - when the simulation is running
                 you can't switch light via Lumos app.
             </MyText>
+            <RkTabView>
+                <RkTabView.Tab title={'Tab 1'}>
+                    <CalendarList
+                        // Callback which gets executed when visible months change in scroll view. Default = undefined
+                        markedDates={this.state.markedDates}
+                        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+                        // Max amount of months allowed to scroll to the past. Default = 50
+                        pastScrollRange={50}
+                        // Max amount of months allowed to scroll to the future. Default = 50
+                        futureScrollRange={50}
+                        // Enable or disable scrolling of calendar list
+                        scrollEnabled={true}
+                        // Enable or disable vertical scroll indicator. Default = false
+                        showScrollIndicator={true}
+                        onDayPress={(day) => {console.log('selected day', day)}}
+                        markingType='multi-period'
+                />
+                </RkTabView.Tab>
+                <RkTabView.Tab title={'Tab 2'}>
+                    <Text>Tab 2 Content</Text>
+                </RkTabView.Tab>
+                <RkTabView.Tab title={'Tab 3'}>
+                    <Text>Tab 3 Content</Text>
+                </RkTabView.Tab>
+            </RkTabView>
             <Button
-                title="Clear button"
-                type="clear"
+                title="Go to Details"
+                onPress={() => this.props.navigation.navigate('Home')}
             />
         </View>;
     }
